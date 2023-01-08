95d4f5467002f1baf754789b75c5789caa1194b3
AidinProgrammer <aidinazamnouri@gmail.com>
2023-01-08 15:43:52 +0100

    Adding social media section and many default themes

--- src/run.py
+++ src/run.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
+st.title(':adult: ReadMe Profile Creator')
+
 # Getting the personal information
-st.title('Personal Info Creator')
+st.header('Personal Info')
 with st.expander('Personal Info'):
     col1, col2 = st.columns(2)
     name = col1.text_input('Name')
@@ -9,3 +11,15 @@ with st.expander('Personal Info'):
     phone = col1.text_input('Phone')
     homepage = col2.text_input('Homepage address')
     location = st.text_input('Location')
+
+# Getting the social media
+st.header('Social Media')
+with st.expander('Social Media'):
+    st.markdown('Please only write your username!')
+    col1, col2 = st.columns(2)
+    github = col1.text_input('GitHub')
+    linkedin = col2.text_input('Linkedin')
+    twitter = col1.text_input('Twitter')
+    facebook = col2.text_input('Facebook')
+    youtube = col1.text_input('YouTube')
+    instagram = col2.text_input('Instagram')
